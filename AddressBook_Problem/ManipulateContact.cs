e000b9f393357ea950c6c8aff430efb944fe1b92
Chilamakooru Gangadevi <chilamakooru.gangaiiit@gmail.com>
2022-04-24 21:43:11 +0530

    Deleting contact

--- AddressBook_Problem/ManipulateContact.cs
+++ AddressBook_Problem/ManipulateContact.cs
@@ -33,7 +33,44 @@ namespace Address_Book_Problem
                     Email = Email
                 }
             );
-            Console.WriteLine($"{Name} is added");
+            Console.WriteLine($"{Name} is added___________");
+        }
+
+
+        public void EditingContact(string Name)
+        {
+            foreach (var contact in AddressBookList)
+            {
+                if (contact.Name.Contains(Name))
+                {
+                    /*Console.WriteLine("plz provide new ");
+                    contact.Address = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.City = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.State = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.ZipCode = Console.ReadLine();*/
+                    Console.WriteLine("plz provide new phone number");
+                    contact.PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("plz provide new email");
+                    contact.Email = Console.ReadLine();
+                    break;
+                }
+            }
+        }
+
+        public void DeletingContact(string Name)
+        {
+            foreach (var contact in AddressBookList)
+            {
+                if (contact.Name.Contains(Name))
+                {
+                    AddressBookList.Remove(contact);
+                    Console.WriteLine($"{Name}'s Contact is deleted______");
+                    break;
+                }
+            }
         }
     }
 }
--- AddressBook_Problem/Program.cs
+++ AddressBook_Problem/Program.cs
@@ -6,21 +6,47 @@ namespace AddressBookSystem
 {
     internal class Program
     {
+        public static ManipulateContact manipulate = new ManipulateContact();
+
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book system");
 
-            ContactDetails detail = new ContactDetails();
-            ManipulateContact AddContact = new ManipulateContact();
-            AddContact.AddingContact(
-                detail.Name = Console.ReadLine(),
-                detail.Address = Console.ReadLine(),
-                detail.City = Console.ReadLine(),
-                detail.State = Console.ReadLine(),
-                detail.ZipCode = Console.ReadLine(),
-                detail.PhoneNumber = Console.ReadLine(),
-                detail.Email = Console.ReadLine()
-                );
+            AddContact();
+            EditContact();
+            DeleteContact();
+        }
+
+        public static void AddContact()
+        {
+            Console.WriteLine("how many contacts do you want to add?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                ContactDetails detail = new ContactDetails();
+                manipulate.AddingContact(
+                    detail.Name = Console.ReadLine(),
+                    detail.Address = Console.ReadLine(),
+                    detail.City = Console.ReadLine(),
+                    detail.State = Console.ReadLine(),
+                    detail.ZipCode = Console.ReadLine(),
+                    detail.PhoneNumber = Console.ReadLine(),
+                    detail.Email = Console.ReadLine()
+                    );
+            }
+        }
+
+        public static void EditContact()
+        {
+            Console.WriteLine("enter name you want edit");
+            string Name = Console.ReadLine();
+            manipulate.EditingContact(Name);
+        }
+        public static void DeleteContact()
+        {
+            Console.WriteLine("enter name you want delete");
+            string Name = Console.ReadLine();
+            manipulate.DeletingContact(Name);
         }
     }
 }
