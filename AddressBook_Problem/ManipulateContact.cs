6320c26e7d00651cf8f24ae9e99120f89002adba
Chilamakooru Gangadevi <chilamakooru.gangaiiit@gmail.com>
2022-04-24 21:07:37 +0530

    Add project files.

---
+++ AddressBook_Problem/ManipulateContact.cs
@@ -0,0 +1,39 @@
+﻿using AddressBook_Problem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Problem
+{
+    internal class ManipulateContact
+    {
+        List<ContactDetails> AddressBookList = new List<ContactDetails>();
+        public void AddingContact
+            (
+            string Name,
+            string Address,
+            string City,
+            string State,
+            string ZipCode,
+            string PhoneNumber,
+            string Email
+            )
+        {
+            AddressBookList.Add
+                (new ContactDetails()
+                {
+                    Name = Name,
+                    Address = Address,
+                    City = City,
+                    State = State,
+                    ZipCode = ZipCode,
+                    PhoneNumber = PhoneNumber,
+                    Email = Email
+                }
+            );
+            Console.WriteLine($"{Name} is added");
+        }
+    }
+}
---
+++ AddressBook_Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using Address_Book_Problem;
+using AddressBook_Problem;
+using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to address book system");
+
+            ContactDetails detail = new ContactDetails();
+            ManipulateContact AddContact = new ManipulateContact();
+            AddContact.AddingContact(
+                detail.Name = Console.ReadLine(),
+                detail.Address = Console.ReadLine(),
+                detail.City = Console.ReadLine(),
+                detail.State = Console.ReadLine(),
+                detail.ZipCode = Console.ReadLine(),
+                detail.PhoneNumber = Console.ReadLine(),
+                detail.Email = Console.ReadLine()
+                );
+        }
+    }
+}
