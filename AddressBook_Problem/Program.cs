aa131d444ddab175e8fbbf427a253823c4aaca4d
Chilamakooru Gangadevi <chilamakooru.gangaiiit@gmail.com>
2022-04-24 21:32:49 +0530

    Editing contacts

--- AddressBook_Problem/Program.cs
+++ AddressBook_Problem/Program.cs
@@ -6,21 +6,44 @@ namespace AddressBookSystem
 {
     internal class Program
     {
+        public static ManipulateContact manipulate = new ManipulateContact();
+
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book system");
 
-            ContactDetails detail = new ContactDetails();
-            ManipulateContact AddContact = new ManipulateContact();
-            AddContact.AddingContact(
-                detail.Name = Console.ReadLine(),
-                detail.Address = Console.ReadLine(),
-                detail.City = Console.ReadLine(),
-                detail.State = Console.ReadLine(),
-                detail.ZipCode = Console.ReadLine(),
-                detail.PhoneNumber = Console.ReadLine(),
-                detail.Email = Console.ReadLine()
-                );
+            AddContact();
+            EditContact();
+        }
+
+        public static void AddContact()
+        {
+            Console.WriteLine("how many contacts do you want to add?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                ContactDetails detail = new ContactDetails();
+                //ManipulateContact AddNewContact = new ManipulateContact();
+                manipulate.AddingContact(
+                    detail.Name = Console.ReadLine(),
+                    detail.Address = Console.ReadLine(),
+                    detail.City = Console.ReadLine(),
+                    detail.State = Console.ReadLine(),
+                    detail.ZipCode = Console.ReadLine(),
+                    detail.PhoneNumber = Console.ReadLine(),
+                    detail.Email = Console.ReadLine()
+                    );
+
+            }
+        }
+
+        public static void EditContact()
+        {
+            //ContactDetails detail = new ContactDetails();
+            //ManipulateContact EditNewContact = new ManipulateContact();
+            Console.WriteLine("enter name you want edit");
+            string Name = Console.ReadLine();
+            manipulate.EditingContact(Name);
         }
     }
 }
